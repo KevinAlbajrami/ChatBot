2fab2507d766903ab3b1d1c0fad0e8e963c18cb8
KevinAlbajrami <58529410+KevinAlbajrami@users.noreply.github.com>
2020-05-29 21:55:44 +0300

    minor bugs

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -15,27 +15,81 @@ import collections
 import nltk
 import pickle
 
-from nltk.stem import WordNetLemmatizer 
-  
-lemmatizer = WordNetLemmatizer() 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 ds=[]
-with open("C:/Users/Kevin Spiceywhinner/Desktop/TestDataset/BanksFAQ.csv", 'r') as read_obj:
-    # pass the file object to reader() to get the reader object
-    csv_reader = reader(read_obj)
-    # Iterate over each row in the csv using reader object
-    for row in csv_reader:
-        # row variable is a list that represents a row in csv
-        ds.append(row)
+with open("C:/Users/Kevin Spiceywhinner/Desktop/TestDataset/dataTrain.csv", 'r',encoding='utf-8') as read_obj:
+	csv_reader = reader(read_obj)
+	for row in csv_reader:
+		ds.append(row)
+		
+			
+# =============================================================================
+# for line in ds:
+#  	if (line[2]==''):
+#  		line[2]=line[1]
+# =============================================================================
+questions_v0=[]
+answers_v0=[]
+for line in ds:
+	questions_v0.append(line[1])
+	answers_v0.append(line[3])
+
+
+answers_v0=answers_v0[:3700]
+questions_v0 = questions_v0[:3700]
+answers_v1=[]
+questions_v1=[]
+str=" "
+for line in answers_v0:
+	if(len(line.split())>19):
+		mid=line.split()[:19]
+		answers_v1.append(str.join(mid))
+	else:
+		answers_v1.append(line)
+
+for line in questions_v0:
+	if(len(line.split())>19):
+		mid=line.split()[:19]
+		questions_v1.append(str.join(mid))
+	else:
+		questions_v1.append(line)
+
+def clean_text(text):
+	text=text.lower()
+	text=re.sub(r"i'm","I am",text)
+	text=re.sub(r"he's","he is",text)
+	text=re.sub(r"she's","she is",text)
+	text=re.sub(r"that's","that is",text)
+	text=re.sub(r"what's","what is",text)
+	text=re.sub(r"<br />","",text)
+	text=re.sub(r"/n"," ",text)
+	text=re.sub(r"\n"," ",text)
+	text=re.sub(r"\'ll"," will",text)
+	text=re.sub(r"\'ve"," have",text)
+	text=re.sub(r"\'re"," are",text)
+	text=re.sub(r"\'d"," would",text)
+	text=re.sub(r"won't","will not",text)
+	text=re.sub(r"can't","cannot",text)
+	text=re.sub(r"https"," ",text)
+	text=re.sub(r"www."," ",text)
+	text=re.sub(r".com"," ",text)
+	text=re.sub(r"ÿ"," ",text)
+	text=re.sub(r"[-()\"#/@;:<>{}+-=.?,|]"," ",text)
+	return text
+
+
 questions=[]
 answers=[]
-for line in ds:
-	questions.append(line[0])
-	answers.append(line[1])
-		
+for question in questions_v1:
+	 question=clean_text(question)
+	 questions.append(question)
 
+for answer in answers_v1:
+	 answer=clean_text(answer)
+	 answers.append(answer)
+	 
 maxlen = 12
 for pos,i in enumerate(answers):
     answers[pos] = re.sub('[^a-zA-Z0-9 .,?!]', '', i)
@@ -70,13 +124,16 @@ for pos,i in enumerate(context):
                 ind = context[pos].index('!')
                 context[pos] = context[pos][:ind+1]
 
+# add Beginning of Sentence (BOS) and End of Sentence (EOS) tags to the 'target' data
+final_target_v0 = ['BOS '+i+' EOS' for i in answers]
+
+final_target_v0 = list(pd.Series(final_target_v0).map(lambda x: re.sub(' +', ' ', x)))
+context = list(pd.Series(context).map(lambda x: re.sub(' +', ' ', x)))
 
 counts = {}
-for words in context+answers:
+for words in context+final_target_v0:
     for word in words.split():
         counts[word] = counts.get(word,0) + 1
-	
-
 	
 
 #dict_words['BOS']=len(dict_words)+1
@@ -84,27 +141,30 @@ for words in context+answers:
 #dict_words['<OUT>']=len(dict_words)+1
 word_to_index = {}
 for pos,i in enumerate(counts.keys()):
-    word_to_index[i] = pos
+	word_to_index[i] = pos
 
 # reverse dictionary mapping indexes to words
-index_to_word = {}
+index_to_word = {}s
 for k,v in word_to_index.items():
     index_to_word[v] = k
 
+
 # apply the dictionary to the context and target data
-final_target = np.array([[word_to_index[w] for w in i.split()] for i in answers])
-context = np.array([[word_to_index[w] for w in i.split()] for i in context])	
+final_target = np.array([[word_to_index[w] for w in i.split()] for i in final_target_v0])
+context = np.array([[word_to_index[w] for w in i.split()] for i in context])
+print(final_target[4])
 
-	 
+
+ 
 # save files
-np.save('context_indexes', context)
+np.save('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/context_indexes', context)
 
-np.save('target_indexes', final_target)
+np.save('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/target_indexes', final_target)
 
-with open('dictionary.pkl', 'wb') as f:
+with open('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/dictionary.pkl', 'wb') as f:
     pickle.dump(word_to_index, f, pickle.HIGHEST_PROTOCOL)
 
-with open('reverse_dictionary.pkl', 'wb') as f:
+with open('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/reverse_dictionary.pkl', 'wb') as f:
     pickle.dump(index_to_word, f, pickle.HIGHEST_PROTOCOL)
 	 
 
--- model.py
+++ model.py
@@ -8,14 +8,14 @@ import numpy as np
 import pickle
 import operator
 from tensorflow.keras.layers import Embedding
-from tensorflow.keras.layers import Input, Dense, LSTM, TimeDistributed
+from tensorflow.keras.layers import Input, Dense, LSTM, TimeDistributed, Bidirectional
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.preprocessing import sequence
 # load the data
-context = np.load('C:/Users/Kevin Spiceywhinner/Desktop/context_indexes.npy',allow_pickle=True)
-final_target = np.load('C:/Users/Kevin Spiceywhinner/Desktop/target_indexes.npy',allow_pickle=True)
-with open('C:/Users/Kevin Spiceywhinner/Desktop/dictionary.pkl', 'rb') as f:
+context = np.load('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/context_indexes.npy',allow_pickle=True)
+final_target = np.load('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/target_indexes.npy',allow_pickle=True)
+with open('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/dictionary.pkl', 'rb') as f:
     word_to_index = pickle.load(f)
 
 
@@ -32,8 +32,8 @@ for k,v in word_to_index.items():
 final_target_1 = final_target
 context_1 = context
 #temp soln: reducing the size of data to be processed
-final_target_1=final_target[:50]
-context_1 = context[:50]
+final_target_1=final_target[:100]
+context_1 = context[:100]
 
 maxLen = 20
 # shift the indexes of the context and target arrays too
@@ -75,12 +75,12 @@ for word,index in word_to_index.items():
 
 outs = np.zeros((context_1.shape[0], maxLen, vocab_size))
 
+
 for pos,i in enumerate(final_target_1):
 	for pos1,j in enumerate(i):
 		if pos1 > 0:
-			outs[pos, pos1 - 1, j] = 1
+			outs[pos, pos1-1, j] = 1
 	if pos%1000 == 0: print ('{} entries completed'.format(pos))
-
 # pad the sequences so that they can be fed into the embedding layer
 final_target_1 = sequence.pad_sequences(final_target_1, maxlen = 20, dtype = 'int32', padding = 'post', truncating = 'post')
 context_1 = sequence.pad_sequences(context_1, maxlen = 20, dtype = 'int32', padding = 'post', truncating = 'post')
@@ -96,23 +96,201 @@ embed_layer.set_weights([embedding_matrix])
 # final dense layer that uses TimeDistributed wrapper to generate 'vocab_size' softmax outputs for each time step in the decoder lstm
 input_context = Input(shape = (maxLen, ), dtype = 'int32', name = 'input_context')
 input_ctx_embed = embed_layer(input_context)
-encoder_lstm, h1, c1 = LSTM(300, return_state = True, return_sequences = True)(input_ctx_embed)
-encoder_lstm2,h2, c2 = LSTM(300, return_state = True, return_sequences = True)(encoder_lstm)
-_,h3, c3 = LSTM(300, return_state = True)(encoder_lstm2)
-encoder_states = [h1, c1, h2, c2,h3,c3]
+encoder_lstm, h1, c1 = LSTM(256, return_state = True, return_sequences = True)(input_ctx_embed)
+_, h3, c3 = LSTM(256, return_state = True,return_sequences = True)(encoder_lstm)
+encoder_states = [h1, c1,h3,c3]
 
 
 input_target = Input(shape = (maxLen, ), dtype = 'int32', name = 'input_target')
 input_tar_embed = embed_layer(input_target)
 # the decoder lstm uses the final states from the encoder lstm as the initial state
-decoder_lstm, context_h, context_c = LSTM(300, return_state = True, return_sequences = True)(input_tar_embed, initial_state = [h1, c1],)
-decoder_lstm2, context_h2, context_c2 = LSTM(300, return_state = True, return_sequences = True)(decoder_lstm, initial_state = [h2, c2],)
-final, context_h3, context_c3 = LSTM(300, return_state = True, return_sequences = True)(decoder_lstm2, initial_state = [h3, c3],)
+decoder_lstm, context_h, context_c = LSTM(256, return_state = True, return_sequences = True)(input_tar_embed, initial_state = [h3,c3],)
+final, context_h3, context_c3 = LSTM(256, return_state = True, return_sequences = True)(decoder_lstm)
 dense_layer=Dense(vocab_size, activation = 'softmax')
 output = TimeDistributed(dense_layer)(final)
-output=Dropout(0.4)(output)
+output=Dropout(0.2)(output)
 model = Model([input_context, input_target], output)
 
 model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 model.summary()
-model.fit([context_1, final_target_1], outs, epochs = 50, batch_size = 32)
+model.fit([context_1, final_target_1], outs, epochs = 100, batch_size = 256)
+
+
+# import required packages
+import numpy as np
+import re
+
+
+# for initial filtering
+maxlen = 12
+maxLen = 20
+
+# import the dictionary
+with open('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/dictionary.pkl', 'rb') as f:
+    word_to_index = pickle.load(f)
+
+# import the reverse dictionary
+with open('C:/Users/Kevin Spiceywhinner/Desktop/Pedro/reverse_dictionary.pkl', 'rb') as f:
+    index_to_word = pickle.load(f)
+
+# the question asked to the chatbot
+question = 'how are you doing'
+
+# preprocessing to make the data into the format required by the model, same as during training
+a = question.split()
+for pos,i in enumerate(a):
+    a[pos] = re.sub('[^a-zA-Z0-9 .,?!]', '', i)
+    a[pos]= re.sub(' +', ' ', i)
+    a[pos] = re.sub('([\w]+)([,;.?!#&\'\"-]+)([\w]+)?', r'\1 \2 \3', i)
+    if len(i.split()) > maxlen:
+            a[pos] = (' ').join(a[pos].split()[:maxlen])
+            if '.' in a[pos]:
+                ind = a[pos].index('.')
+                a[pos] = a[pos][:ind+1]
+            if '?' in a[pos]:
+                ind = a[pos].index('?')
+                a[pos] = a[pos][:ind+1]
+            if '!' in a[pos]:
+                ind = a[pos].index('!')
+                a[pos] = a[pos][:ind+1]
+
+question = ' '.join(a).split()
+
+# make the question into an array of the corresponding indexes
+question = np.array([word_to_index[w] for w in question])
+
+# pad sequences
+question = sequence.pad_sequences([question], maxlen = 20)
+
+# Keras model used to train, so that we define the variables (tensors) that ultimately go into the infernce model
+#input_context = Input(shape = (maxLen, ), dtype = 'int32', name = 'input_context')
+#input_target = Input(shape = (maxLen, ), dtype = 'int32', name = 'output_context')
+
+#input_ctx_embed = embed_layer(input_context)
+#input_tar_embed = embed_layer(input_target)
+
+#encoder_lstm, context_h, context_c = LSTM(256, return_state = True, return_sequences = True)(input_ctx_embed)
+#encoder_lstm1, context_h1, context_c1 = LSTM(256, return_state = True, return_sequences = True)(encoder_lstm)
+#
+#decoder_lstm, h, _ = LSTM(256, return_state = True, return_sequences = True)(input_tar_embed, initial_state = [context_h1, context_c1],)
+#decoder_lstm1, h1, _ = LSTM(256, return_state = True, return_sequences = True)(decoder_lstm)
+
+#output = Dense(vocab_size, activation = 'softmax')(decoder_lstm1)
+
+# Define the model for the input (question). Returns the final state vectors of the encoder LSTM
+context_model = Model(input_context, encoder_states)
+
+# define the inputs for the decoder LSTM
+target_h = Input(shape = (256, ))
+target_c = Input(shape = (256, ))
+target_h2 = Input(shape = (256, ))
+target_c2 = Input(shape = (256, ))
+decoder_states_inputs = [target_h, target_c,target_h2,target_c2]
+# the decoder LSTM. Takes in the embedding of the initial word passed as input into the decoder model (the 'BOS' tag), 
+# along with the final states of the encoder model, to output the corresponding sequences for 'BOS', and the new LSTM states.  
+target, h, c = LSTM(256, return_state = True, return_sequences = True)(input_tar_embed, initial_state = decoder_states_inputs[0:2])
+target2, h2, c2 = LSTM(256, return_state = True, return_sequences = True)(target, initial_state=decoder_states_inputs[-2:])
+decoder_states=[h,c,h2,c2]
+dec_output = Dense(vocab_size, activation = 'softmax')(target2)
+target_model = Model(
+[input_target] + decoder_states_inputs,
+[dec_output] + decoder_states)
+#target_model = Model([input_target, target_h, target_c], [output, h2, c2])
+target_model.summary()
+# pass in the question to the encoder LSTM, to get the final encoder states of the encoder LSTM
+states_value= context_model.predict(question)
+
+#reverse_input_char_index = dict(
+#    (i, char) for char, i in index_to_word.items())
+#reverse_target_char_index = dict(
+#    (i, char) for char, i in index_to_word.items())
+# i keeps track of the length of the generated answer. This won't allow the model to genrate sequences with more than 20 words.
+answer = np.zeros((1, maxLen))
+answer[0, -1] = word_to_index['BOS']
+
+i = 1
+
+# make a new list to store the words generated at each time step
+answer_1 = []
+
+# flag to stop the model when 'EOS' tag is generated or when 20 time steps have passed.
+flag = 0
+
+# run the inference model
+while flag != 1:
+    # make predictions for the given input token and encoder states
+    prediction, h, c, h1, c1 = target_model.predict(
+            [answer] + states_value)
+    # from the generated predictions of shape (num_examples, maxLen, vocab_size), find the token with max probability
+    token_arg = np.argmax(prediction[0, -1, :])
+    
+    # append the corresponding word of the index to the answer_1 list
+    answer_1.append(index_to_word[token_arg])
+    
+    # set flag to 1 if 'EOS' token is generated or 20 time steps have passed
+    if token_arg == word_to_index['EOS'] or i > 20:
+        flag = 1
+    # re-initialise the answer variable, and set the last token to the output of the current time step. This is then passed
+    # as input to the next time step, along with the LSTM states of the current time step
+    answer = np.zeros((1,maxLen))
+    answer[0, -1] = token_arg
+    states_value = [h, c, h1, c1]
+    
+    # increment the count of the loop
+    i+=1
+    
+ # print the answer generated for the given question
+print (' '.join(answer_1))
+# =============================================================================
+# def decode_sequence(input_seq, encoder_model, decoder_model):
+#     # Encode the input as state vectors.
+#     states_value = encoder_model.predict(input_seq)
+# 
+#     # Generate empty target sequence of length 1.
+#     target_seq = np.zeros((1, 1, maxLen))
+#     # Populate the first character of target sequence with the start character.
+#     target_seq[0, 0, index_to_word['BOS']] = 1.
+# 
+#     # Sampling loop for a batch of sequences
+#     # (to simplify, here we assume a batch of size 1).
+#     stop_condition = False
+#     decoded_sentence = []  #Creating a list then using "".join() is usually much faster for string creation
+#     while not stop_condition:
+#         to_split = decoder_model.predict([target_seq] + states_value)
+# 
+#         output_tokens, states_value = to_split[0], to_split[1:]
+# 
+#         # Sample a token
+#         sampled_token_index = np.argmax(output_tokens[0, 0])
+#         sampled_char = index_to_word[sampled_token_index]
+#         decoded_sentence.append(sampled_char)
+# 
+#         # Exit condition: either hit max length
+#         # or find stop character.
+#         if sampled_char == 'EOS' or len(decoded_sentence) > 20:
+#             stop_condition = True
+# 
+#         # Update the target sequence (of length 1).
+#         target_seq = np.zeros((1, 1, maxLen))
+#         target_seq[0, 0, sampled_token_index] = 1.
+# 
+#     return "".join(decoded_sentence)
+# =============================================================================
+
+
+# =============================================================================
+# for seq_index in range(20):
+#     # Take one sequence (part of the training set)
+#     # for trying out decoding.
+#     input_seq = input_context[seq_index: seq_index + 1]
+#     decoded_sentence = decode_sequence(question,context_model,target_model)
+#     #print('-')
+#    # print('Input sentence:', input_texts[seq_index])
+#     #print('Target sentence:', target_texts[seq_index])
+#     print('Decoded sentence:', decoded_sentence)
+# # initialize the answer that will be generated for the 'BOS' input. Since we have used pre-padding for padding sequences,
+# # the last token in the 'answer' variable is initialised with the index for 'BOS'.
+# answer = np.zeros((1, maxLen))
+# answer[0, -1] = word_to_index['BOS']
+# =============================================================================
+
